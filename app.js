d479203184f2940d7d0f3df8583bd0aacb8d24ea
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-05-03 09:49:50 +0530

    code review

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Function with no args to sum all numbers.
 function findSum() {
-	var args = arguments; // Var to store agruments array.
-	var sumOfNos = 0; // Var to store the sum of all the arguments.
+  var args = arguments; // Var to store agruments array.
+  var sumOfNos = 0; // Var to store the sum of all the arguments.
 
-	for (arg in args) { // Iterate all args through for in loop.
-      sumOfNos += args[arg];  // Sum up operation.
-	}
+  for (arg in args) { // Iterate all args through for in loop.
+    sumOfNos += args[arg];  // Sum up operation.
+  }
 
-	return sumOfNos; // Returning the final output.
+  return sumOfNos; // Returning the final output.
 }
 
 console.log(findSum(1,2,3)); // Prints 6.
