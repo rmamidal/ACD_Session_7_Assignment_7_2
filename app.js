dace1a0c82b20aaef5b6e9ba17d2ef22ffadb4ef
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-05-03 09:48:44 +0530

    function with no arg to find sum of all no

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// Function to return Area of circle.
-function calculateArea(r) {
-  return 3.14 * r * r; // Area of circle formula.
+// Function with no args to sum all numbers.
+function findSum() {
+	var args = arguments; // Var to store agruments array.
+	var sumOfNos = 0; // Var to store the sum of all the arguments.
+
+	for (arg in args) { // Iterate all args through for in loop.
+      sumOfNos += args[arg];  // Sum up operation.
+	}
+
+	return sumOfNos; // Returning the final output.
 }
 
-
-console.log("Area of Circle: "+ Math.ceil(calculateArea(7))); // Prints Area of Circle with round off.
+console.log(findSum(1,2,3)); // Prints 6.
+console.log(findSum(1,2,3,4,5,6,7,8,9,10)); // Prints 55.
 
