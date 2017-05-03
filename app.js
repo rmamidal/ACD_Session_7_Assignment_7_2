5038a95c3e8a5ff0b0fbb86fac4ec4cd6ece962d
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-05-03 09:47:54 +0530

    function with no args to find sum of all no

---
+++ app.js
@@ -0,0 +1,8 @@
+// Function to return Area of circle.
+function calculateArea(r) {
+  return 3.14 * r * r; // Area of circle formula.
+}
+
+
+console.log("Area of Circle: "+ Math.ceil(calculateArea(7))); // Prints Area of Circle with round off.
+
